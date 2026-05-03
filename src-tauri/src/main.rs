aca650e2eeaee0fa6852f3af80f6d383a367e6bf
BangZ <i@bangz.me>
2026-05-03 17:29:06 +0800

    Add Windows MSVC build support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Prevents an additional console window on Windows for both debug and release builds.
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 
 fn main() {
     pff_explorer_lib::run()
