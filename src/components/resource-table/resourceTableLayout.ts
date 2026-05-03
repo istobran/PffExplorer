4ba59185d32acca213fac2985902879bf433b8a5
BangZ <i@bangz.me>
2026-05-03 16:56:11 +0800

    Widen resource table row number column

--- src/components/resource-table/resourceTableLayout.ts
+++ src/components/resource-table/resourceTableLayout.ts
@@ -1,7 +1,7 @@
 export const RESOURCE_TABLE_ROW_HEIGHT = 24;
 
 export const RESOURCE_TABLE_COLUMNS =
-  "44px minmax(220px, 1fr) 68px 68px 100px 100px";
+  "60px minmax(220px, 1fr) 68px 68px 100px 100px";
 
 export const RESOURCE_TABLE_COLUMNS_WITH_ARCHIVE =
-  "44px minmax(180px, 1fr) minmax(118px, 150px) 68px 68px 100px 100px";
+  "60px minmax(180px, 1fr) minmax(118px, 150px) 68px 68px 100px 100px";
